858556ddd292f95ed73d9956087b48b4f23b2fae
Zoom30 <63873134+Zoom30@users.noreply.github.com>
2021-08-13 17:58:00 +0100

    start flask project

--- Day 54/flask_app.py
+++ Day 54/flask_app.py
@@ -1,8 +1,22 @@
+import time
+
 from flask import Flask
 
 app = Flask(__name__)
+
+print(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def say_bye():
+    return "bye"
+
+
+if __name__ == "__main__":
+    app.run()
+    
+
