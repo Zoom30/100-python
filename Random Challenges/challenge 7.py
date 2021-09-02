89fdb15f4fa49a9acbf896447206b6be99521546
Daniel <danielabraha99@gmail.com>
2021-09-02 22:22:27 +0100

    remove sample matrix

--- Random Challenges/challenge 7.py
+++ Random Challenges/challenge 7.py
@@ -34,12 +34,6 @@ def determinant(matrix):
         return matrix[0][0]
     elif len(matrix) == 2:
         return ((matrix[0][0] * matrix[1][1]) - (matrix [0][1] * matrix[1][0]))
-    # m2 = [ 
-    #     [2,5,3], 
-    #     [1,-2,-1], 
-    #     [1, 3, 4]
-    #     ]
-    # matrix = m2
 
     else:
         return (round(np.linalg.det(np.array(matrix))))
