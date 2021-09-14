8bd24853c7cde8d3048ff9bd2c76ec94095cda5f
Daniel <danielabraha99@gmail.com>
2021-09-14 21:52:07 +0100

    more on flask basics

--- Day 55/main.py
+++ Day 55/main.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def greetings():
-    return "greetings hello"
+    return "<h1 style='text-align:center'>greetings hello</h1>" \
+           "<p>This is a paragraph</p>" \
+           "<img src = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2021/05/25082143/Alaskan-Malamute-puppy-laying-down-outdoors.jpg'>"
 
 
-@app.route("/username/<path:name>")
-def greet(name):
-    return f"hello {name}"
+@app.route("/username/<path:name>/<int:number>")
+def greet(name, number):
+    return f"hello {name}, you are {number} years old"
 
 
 if __name__ == "__main__":
